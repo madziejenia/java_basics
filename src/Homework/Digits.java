24b893e75dd804f9439c3259159ebf6e3516722b
Magdalena Krupska <magdalena.krupska@o2.pl>
2019-03-04 22:46:09 +0100

    all files

--- src/Homework/Digits.java
+++ src/Homework/Digits.java
@@ -2,14 +2,14 @@ package Homework;
 
 public class Digits {
 
-    static int NumberofDigits(int n) {
+    static int NumberOfDigits(int n) {
         if (n == 0)
             return 0;
-        return 1 + NumberofDigits(n / 10);
+        return 1 + NumberOfDigits(n / 10);
     }
 
     public static void main(String[] args) {
         int n = 845739604;
-        System.out.println("Number of digits: " + NumberofDigits(n));
+        System.out.println("Number of digits: " + NumberOfDigits(n));
     }
 }
